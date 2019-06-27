84317ccff830ef1463ced1238019f068754d698c
Oscar Lopez <olopez@xpertcode.com.do>
2019-06-27 14:23:53 -0400

    feat: Login

---
+++ pages/login.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="container">
+    <img
+      class="max-w-sm"
+      src="http://xpertcode.com.do/wp-content/uploads/2018/08/XC-logo-final-05.png"
+      alt="logo"
+    >
+
+    <div class="flex mt-12">
+      <div class>
+        <img
+          class="pt-20 pr-20 my-0 mx-auto"
+          src="../assets/images/undraw_authentication_fsn5.svg"
+          alt
+        >
+      </div>
+
+      <div>
+        <h1 class="text-5xl text-teal-600 font-bold">Bienvenido</h1>
+        <p
+          class="text-md text-gray-800 mt-5"
+        >Puedes utilizar esta aplicacion para reportar tus gastos a ser rembolsables por nomina.</p>
+
+        <div class="flex mt-20 items-center border border-gray-200 border-b-0 rounded-t-lg">
+          <div class="ml-3 p-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="21.327"
+              viewBox="0 0 30 21.327"
+            >
+              <g id="Group_574" data-name="Group 574" transform="translate(-852.156 -479.401)">
+                <g id="Group_551" data-name="Group 551">
+                  <path
+                    id="Path_553"
+                    data-name="Path 553"
+                    d="M876.446,495.873a.48.48,0,0,1-.331-.132l-5.566-5.221a.484.484,0,0,1,.663-.7l5.566,5.222a.484.484,0,0,1-.332.836Z"
+                    fill="#999"
+                  ></path>
+                  <path
+                    id="Path_554"
+                    data-name="Path 554"
+                    d="M857.866,495.873a.484.484,0,0,1-.332-.836l5.568-5.222a.484.484,0,0,1,.663.7l-5.568,5.221A.48.48,0,0,1,857.866,495.873Z"
+                    fill="#999"
+                  ></path>
+                </g>
+                <path
+                  id="Path_555"
+                  data-name="Path 555"
+                  d="M879.738,500.728H854.575a2.421,2.421,0,0,1-2.419-2.418V481.819a2.421,2.421,0,0,1,2.419-2.418h25.163a2.42,2.42,0,0,1,2.418,2.418V498.31A2.42,2.42,0,0,1,879.738,500.728Zm-25.163-20.36a1.453,1.453,0,0,0-1.451,1.451V498.31a1.453,1.453,0,0,0,1.451,1.451h25.163a1.453,1.453,0,0,0,1.451-1.451V481.819a1.453,1.453,0,0,0-1.451-1.451Z"
+                  fill="#999"
+                ></path>
+                <path
+                  id="Path_556"
+                  data-name="Path 556"
+                  d="M867.156,492.495a2.709,2.709,0,0,1-1.776-.64l-12.421-10.836a.484.484,0,1,1,.636-.729l12.421,10.834a1.81,1.81,0,0,0,2.28,0l12.4-10.817a.484.484,0,0,1,.637.729l-12.406,10.818A2.7,2.7,0,0,1,867.156,492.495Z"
+                  fill="#999"
+                ></path>
+              </g>
+            </svg>
+          </div>
+          <div class="w-full py-4">
+            <span class="block text-xs text-gray-800">Email address</span>
+            <input
+              class="w-full focus:outline-none"
+              name="email"
+              placeholder="dtrump@xpertcode.com.do"
+            >
+          </div>
+
+          <div class="ml-auto">
+            <svg
+              class="mr-5"
+              xmlns="http://www.w3.org/2000/svg"
+              width="23.167"
+              height="23.167"
+              viewBox="0 0 23.167 23.167"
+            >
+              <g transform="translate(-1155.489 -478.107)">
+                <circle
+                  cx="11.584"
+                  cy="11.584"
+                  r="11.584"
+                  transform="translate(1155.489 478.107)"
+                  fill="#fff"
+                ></circle>
+                <path
+                  d="M1167.073,479.395a10.3,10.3,0,1,0,10.3,10.3A10.3,10.3,0,0,0,1167.073,479.395Zm-1.889,15.585-4.533-4.534,1.511-1.511,3.022,3.022,6.8-6.8,1.511,1.511Z"
+                  fill="#15824b"
+                ></path>
+              </g>
+            </svg>
+          </div>
+        </div>
+
+        <div class="flex items-center border border-gray-200 rounded-b-lg">
+          <div class="ml-3 p-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="26"
+              height="31.909"
+              viewBox="0 0 26 31.909"
+            >
+              <g transform="translate(-852.656 -544.365)">
+                <path
+                  d="M875.111,556.25v-3.317c0-4.724-4.242-8.568-9.455-8.568s-9.454,3.844-9.454,8.568v3.317a4.276,4.276,0,0,0-3.546,4.206V572a4.277,4.277,0,0,0,4.272,4.271h17.457A4.276,4.276,0,0,0,878.656,572V560.455A4.275,4.275,0,0,0,875.111,556.25Zm-17.727-3.317c0-4.073,3.711-7.386,8.272-7.386s8.273,3.313,8.273,7.386v3.25H857.384ZM877.475,572a3.094,3.094,0,0,1-3.09,3.09H856.928a3.093,3.093,0,0,1-3.09-3.09V560.455a3.093,3.093,0,0,1,3.09-3.09h17.457a3.094,3.094,0,0,1,3.09,3.09Z"
+                  fill="#999"
+                ></path>
+                <path
+                  d="M865.656,560.911a2.366,2.366,0,0,0-2.363,2.363v3.546a2.363,2.363,0,0,0,4.727,0v-3.546A2.366,2.366,0,0,0,865.656,560.911Zm1.182,5.909a1.182,1.182,0,0,1-2.363,0v-3.546a1.182,1.182,0,1,1,2.363,0Z"
+                  fill="#999"
+                ></path>
+              </g>
+            </svg>
+          </div>
+          <div class="w-full py-4">
+            <span class="block text-xs text-gray-800">Password</span>
+            <input
+              class="w-full focus:outline-none"
+              type="password"
+              name="password"
+              placeholder="password"
+            >
+          </div>
+        </div>
+
+        <div class="flex flex-col items-center">
+          <a class="mt-4 py-0 text-gray-500 px-auto" href="#">¿Olvidaste tu constraseña?</a>
+
+          <a class="block mt-10 btn btn-grad" href>Iniciar Sesión</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+
